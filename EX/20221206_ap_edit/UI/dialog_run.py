a1a2f50cfbbcc73318b9a05704ed235ac656a100
garyhsieh15 <garyhsieh15@gmail.com>
2022-12-08 01:27:01 +0800

    add new sub win

---
+++ EX/20221206_ap_edit/UI/dialog_run.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'dialog_run.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QSizePolicy, QWidget)
+
+class Ui_Dialog_Run(object):
+    def setupUi(self, Dialog_Run):
+        if not Dialog_Run.objectName():
+            Dialog_Run.setObjectName(u"Dialog_Run")
+        Dialog_Run.resize(400, 300)
+
+        self.retranslateUi(Dialog_Run)
+
+        QMetaObject.connectSlotsByName(Dialog_Run)
+    # setupUi
+
+    def retranslateUi(self, Dialog_Run):
+        Dialog_Run.setWindowTitle(QCoreApplication.translate("Dialog_Run", u"Dialog", None))
+    # retranslateUi
+
--- EX/20221206_ap_edit/main.py
+++ EX/20221206_ap_edit/main.py
@@ -1,18 +1,37 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
+
+from PySide6 import QtCore
+from PySide6.QtCore import Qt, QObject
 from PySide6.QtCore import QFile
+
 #    folder.file     class
 from UI.label import Ui_MainWindow
-#     folder    file
-#from UI import label
+from UI.dialog_run import Ui_Dialog_Run
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
         self.ui = Ui_MainWindow()
-        #self.ui = label.Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui_dialog_run = DialogRun()
+        self.ui.actionRun.triggered.connect(self.popup)
+
+    @QtCore.Slot()
+    def popup(self):
+        ret = self.ui_dialog_run.show()
+        #print(ret)
+
+
+class DialogRun(QMainWindow):
+    def __init__(self):
+        super(DialogRun, self).__init__()
+
+        self.dialog_run = Ui_Dialog_Run()
+        self.dialog_run.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
